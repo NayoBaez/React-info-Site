cb2243735146d0d60ce881a9a31bdb3bd6f51766
NayoBaez <104393006+NayoBaez@users.noreply.github.com>
2022-11-21 17:57:43 -0400

    Fixed Nav Icon error

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,10 @@
 import React from "react";
+import Logo from "./images/react-icon-small.png";
 
 export default function Navbar() {
   return (
     <nav>
-      <img
-        src="../images/react-icon-small.png"
-        alt="react-logo"
-        className="nav--icon"
-      />
-      <h3 className="nav--logo_text">ReactFacts</h3>
+      <img src={Logo} alt="react-logo" className="nav--icon" />
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
   );
